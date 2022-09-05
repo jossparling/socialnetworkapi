fa3f5918ed125205668753b573e9a7a60e0f2b63
jossparling <jossparling@gmail.com>
2022-09-05 14:06:10 -0500

    added models

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const {Schema, model} = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtTest: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions:
+        [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('ractionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
