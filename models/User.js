84dba1e292b54cd11423c049bd188f0e8c61a798
jossparling <jossparling@gmail.com>
2022-09-07 17:29:07 -0500

    user routes working

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-        match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        match: [/.+@.+\..+/, 'Must match an email address!']
     },
     thoughts: [
         {
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,36 +1,9 @@
 const router = require('express').Router();
-const mongoose = require('../../config/connection');
-const { User, Thought } = require('../../models');
 
-// get all users
-router.get('/', (req, res) => {
-    Post.findall({
-        attributes: [
-            'username',
-            'email',
-            'thought',
-            'friends'
-        ],
-        include: [
-            {
-                model: Thought,
-                attributes: ['thoughtTest', 'createdAt', 'username', 'reactions'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+const {
+    getUsers, createUser
+} = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
 
 module.exports = router;
